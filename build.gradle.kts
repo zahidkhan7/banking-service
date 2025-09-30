d0ff72ac5f90ff25db79730bfed4eba9468c740c
Zahid Khan <certainzkhan@gmail.com>
2025-09-30 13:34:24 +0100

    Added build file

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.6"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.zad.eng.excellence.poc"
+version = "1.0"
+description = "POC Project for Banking Service"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
